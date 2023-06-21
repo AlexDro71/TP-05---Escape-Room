a1d4ddad2155b4b1bf70c9e4dbe78054607a8789
SIMÓN SUKEN ROSENBERG <47296430@ort>
2023-06-21 09:06:01 -0300

    Avances en las views

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPBase.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult tutorial()
+    {
+
+        return View();
+    }
+
+        public IActionResult creditos()
+    {
+
+        return View();
+    }
+    public IActionResult Comenzar()
+    {
+        int numSala = Escape.getEstadoJuego();
+        return View("sala" + (numSala + 1));
+    }
+
+    
+    public IActionResult Habitacion(int sala, string clave)
+    {
+        
+if(sala!=Escape.getEstadoJuego()){
+return View("sala"+Escape.getEstadoJuego());
+}else if(!Escape.ResolverSala(sala, clave)){
+    ViewBag.Error="El codigo no es correcto, vuelva a intentar";
+return View("sala"+Escape.getEstadoJuego());
+}else if(Escape.ResolverSala(sala, clave) && Escape.getEstadoJuego()==4){
+return View("escapaste");
+}else{
+    return View();
+}
+
+    }
+}
+
